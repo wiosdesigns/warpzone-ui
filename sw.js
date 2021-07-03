94b8efe322ef3492ce05a98c3cc6485abc5ef6ba
Karthik D <krtdvn@gmail.com>
2021-07-03 18:56:28 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request);
-      });
+      })
     );
   }
 });
